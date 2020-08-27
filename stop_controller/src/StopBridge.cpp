365187b3e956fbdf8c7f46a88e7db41193646976
Andreas Zachariae <zaan1017@hs-karlsruhe.de>
2020-08-27 13:23:33 +0200

    Added documentation

--- stop_controller/src/StopBridge.cpp
+++ stop_controller/src/StopBridge.cpp
@@ -1,3 +1,12 @@
+/** *******************************************************
+ * PeTRA - University of Applied Sciences Karlsruhe
+ * Module : ROS1-Node "StopBridge"
+ * Purpose : Converts topic "Stop" to "move_base/cancel"
+ *
+ * @author Moritz Weisenböhler
+ * @author Andreas Zachariae
+ * @since 1.0.0 (2020.08.26)
+ *********************************************************/
 #include <ros/ros.h>
 
 #include <std_msgs/Empty.h>
@@ -5,20 +14,20 @@
 
 class StopBridge
 {
+public:
+    StopBridge(ros::NodeHandle n);
+
 private:
     ros::NodeHandle handle_;
     ros::Publisher cancel_action_pub_ = handle_.advertise<actionlib_msgs::GoalID>("move_base/cancel", 1000);
     ros::Subscriber stop_sub_ = handle_.subscribe("Stop", 1000, &StopBridge::stop_callback_, this);
 
     void stop_callback_(const std_msgs::Empty::ConstPtr &msg);
-
-public:
-    StopBridge(ros::NodeHandle n);
-
 };
 
 StopBridge::StopBridge(ros::NodeHandle n) : handle_(n)
 {
+    ROS_INFO("StopBridge active, bridging /Stop to /move_base/cancel");
 }
 
 void StopBridge::stop_callback_(const std_msgs::Empty::ConstPtr &msg)
